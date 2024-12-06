f3c78d1da170ed46a76fa8936648b135c07e3eab
nltvn <Tijs.van.Noije@vanderlande.com>
2024-12-06 15:50:10 +0100

    day 6

---
+++ aoc/src/main/java/Day5.java
@@ -0,0 +1,135 @@
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Day5 {
+
+    static List<Pair<Integer, Integer>> rulePairs = new ArrayList<>();
+    static List<List<Integer>> validBooks = new ArrayList<>();
+    static List<List<Integer>> inValidBooks = new ArrayList<>();
+
+    private static int part1() {
+
+        int total = 0;
+
+        for (var book : validBooks) {
+            total += getValueOfMiddlePage(book);
+        }
+
+        return total;
+    }
+
+    private static int part2() {
+
+        int total = 0;
+
+        for (var book : inValidBooks) {
+            total += getValueOfMiddlePage(generateValidBook(book));
+        }
+
+        return total;
+    }
+
+    private static boolean isBookValid(List<Integer> book) {
+
+        for (var rulePair : rulePairs) {
+
+            int i = book.indexOf(rulePair.key());
+            int j = book.indexOf(rulePair.value());
+
+            if ((i == -1) || (j == -1)) {
+                continue;
+            }
+
+            if (i > j) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static Integer getValueOfMiddlePage(List<Integer> book) {
+        int size = book.size();
+        return book.get(size/2);
+    }
+
+    private static void readRules(List<String> input) {
+        final String regex = "(\\d+)\\|(\\d+)";
+
+        final Pattern pattern = Pattern.compile(regex, Pattern.MULTILINE);
+        final Matcher matcher = pattern.matcher(Util.stringsToString(input));
+
+        while (matcher.find()) {
+
+            int first = Integer.parseInt(matcher.group(1));
+            int second = Integer.parseInt(matcher.group(2));
+
+            rulePairs.add(new Pair<>(first, second));
+        }
+    }
+
+    private static void createBooks(List<String> input) {
+        List<List<Integer>> books = new ArrayList<>();
+
+        input.forEach(line -> {
+            if (!line.contains(",")) {
+                return;
+            }
+
+            List<Integer> book = new ArrayList<>();
+            String[] bookPages = line.split(",");
+
+            Arrays.stream(bookPages).forEach(page -> book.add(Integer.parseInt(page)));
+
+            books.add(book);
+
+            if (isBookValid(book)) {
+                validBooks.add(book);
+            } else {
+                inValidBooks.add(book);
+            }
+
+        });
+    }
+
+    private static List<Integer> generateValidBook(List<Integer> book) {
+
+        List<Integer> newBook = new ArrayList<>();
+
+        for (var page : book) {
+
+            int i = 0;
+            newBook.add(page);
+
+            while (!isBookValid(newBook)) {
+                newBook.remove(page);
+
+                newBook.add(i, page);
+                i++;
+            }
+        }
+
+        return newBook;
+    }
+
+    public static void main(String[] args) throws IOException {
+        long startTime = System.currentTimeMillis();
+        List<String> input = Util.readFile(".\\aoc\\src\\main\\resources\\day5.txt");
+
+        readRules(input);
+        createBooks(input);
+
+        System.out.printf("D5 P1 = %s%n", part1());
+        long endtime = System.currentTimeMillis();
+        System.out.println("D5 P1 took " + (endtime - startTime) + " milliseconds");
+        System.out.println();
+
+        startTime = System.currentTimeMillis();
+        System.out.printf("D5 P2 = %s%n", part2());
+        endtime = System.currentTimeMillis();
+        System.out.println("D5 P2 took " + (endtime - startTime) + " milliseconds");
+    }
+}
---
+++ aoc/src/main/java/Day6.java
@@ -0,0 +1,173 @@
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+public class Day6 {
+
+    static final int LOOP_LIMIT = 8000;
+    static Pair<Integer, Integer> currentLocation;
+    static Pair<Integer, Integer> initialLocation;
+
+    public enum Direction {
+        NORTH,
+        EAST,
+        SOUTH,
+        WEST
+    }
+
+    static Direction heading;
+    static Direction initialHeading;
+
+    static final Map<Direction, Pair<Integer, Integer>> headingMutation = Map.of(
+        Direction.NORTH, new Pair<>(-1, 0),
+        Direction.EAST, new Pair<>(0, 1),
+        Direction.SOUTH, new Pair<>(1, 0),
+        Direction.WEST, new Pair<>(0, -1)
+    );
+
+    static List<String> map;
+    static List<String> initialMap;
+
+    private static long part1() {
+
+        patrol();
+
+        return countVisitedLocations();
+    }
+
+    private static long part2() {
+
+        long loopsFound = 0;
+
+        for (var i = 0; i < map.size(); i++) {
+            for (var j = 0; j < map.get(i).length(); j++) {
+                if (map.get(i).charAt(j) == '.') {
+                    addNewObstacle(new Pair<>(i, j));
+
+                    if (!patrol()) {
+                        loopsFound++;
+                    }
+
+                    setPlayField();
+                }
+            }
+        }
+
+        return loopsFound;
+    }
+
+
+    private static boolean patrol() {
+
+        int steps = 0;
+
+        Pair<Integer, Integer> nextLocation = getNextLocation(currentLocation, heading);
+
+        while (steps < LOOP_LIMIT) {
+            steps++;
+
+            try {
+                markLocationAsVisited(currentLocation);
+
+                while (getIconOnLocation(nextLocation).equals("#")) {
+                    changeHeadingDirection();
+                    nextLocation = getNextLocation(currentLocation, heading);
+                }
+
+                currentLocation = nextLocation;
+
+                nextLocation = getNextLocation(currentLocation, heading);
+
+            } catch (Exception e) {
+                return true;
+            }
+        }
+
+        return steps != LOOP_LIMIT;
+    }
+
+    private static void findStartConditions(List<String> input) {
+
+        Map<String, Direction> headingIcon = Map.of(
+                ">", Direction.EAST,
+                "<", Direction.WEST,
+                "^", Direction.NORTH,
+                "v", Direction.SOUTH
+        );
+
+        initialMap = input;
+
+        for (var i = 0; i < input.size(); i++) {
+            for (var j = 0; j < input.get(i).length(); j++) {
+
+                String current = String.valueOf(input.get(i).charAt(j));
+
+                if (headingIcon.containsKey(current)) {
+                    initialLocation = new Pair<>(i, j);
+                    initialHeading = headingIcon.get(current);
+                    setPlayField();
+                    return;
+                }
+            }
+        }
+    }
+
+    private static String getIconOnLocation(Pair<Integer, Integer> location) {
+        return String.valueOf(map.get(location.key()).charAt(location.value()));
+    }
+
+    private static Pair<Integer, Integer> getNextLocation(Pair<Integer, Integer> currentLocation, Direction heading) {
+        Pair<Integer, Integer> integerIntegerPair = headingMutation.get(heading);
+        return new Pair<>(currentLocation.key() + integerIntegerPair.key(), currentLocation.value() + integerIntegerPair.value());
+    }
+
+    private static void markLocationAsVisited(Pair<Integer, Integer> location) {
+        map.set(location.key(), map.get(location.key()).substring(0, location.value()) + "X" + map.get(location.key()).substring(location.value() + 1));
+    }
+
+    private static void addNewObstacle(Pair<Integer, Integer> location) {
+        map.set(location.key(), map.get(location.key()).substring(0, location.value()) + "#" + map.get(location.key()).substring(location.value() + 1));
+    }
+
+    private static long countVisitedLocations() {
+        long total = 0;
+        for (var row : map) {
+            total += row.chars().filter(ch -> ch == 'X').count();
+        }
+        return total;
+    }
+
+    private static void setPlayField() {
+        map = new ArrayList<>(initialMap);
+        currentLocation = new Pair<>(initialLocation.key(), initialLocation.value());
+        heading = initialHeading;
+    }
+
+    private static void changeHeadingDirection() {
+        switch (heading) {
+            case NORTH -> heading = Direction.EAST;
+            case EAST -> heading = Direction.SOUTH;
+            case SOUTH -> heading = Direction.WEST;
+            case WEST -> heading = Direction.NORTH;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        long startTime = System.currentTimeMillis();
+        List<String> input = Util.readFile(".\\aoc\\src\\main\\resources\\day6.txt");
+
+        findStartConditions(input);
+
+        System.out.printf("D6 P1 = %s%n", part1());
+        long endtime = System.currentTimeMillis();
+        System.out.println("D6 P1 took " + (endtime - startTime) + " milliseconds");
+        System.out.println();
+
+        startTime = System.currentTimeMillis();
+        System.out.printf("D6 P2 = %s%n", part2());
+        endtime = System.currentTimeMillis();
+        System.out.println("D6 P2 took " + (endtime - startTime) + " milliseconds");
+    }
+}
